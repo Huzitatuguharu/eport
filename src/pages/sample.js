00861b94f12c9146c39836a1764549688179bfb0
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-09-07 15:46:24 +0900

    ピン変更

--- src/pages/sample.js
+++ src/pages/sample.js
@@ -1,5 +1,5 @@
-// import { useState, useEffect } from 'react';
-// import useSWR, { SWRConfig } from 'swr';
+import { useState, useEffect } from 'react';
+import useSWR, { SWRConfig } from 'swr';
 
 // import { supabase } from '../lib/createSupabaseClient';
 
@@ -14,37 +14,24 @@
 //   };
 // }
 
-// export default function FirstPost() {
-//   const { data, error } = useSWR('./api/airport.ts', fetcher);
+export default function FirstPost() {
+  return (
+    <div>
+      <Children theme={{ background: 'yellow' }} large />
+    </div>
+  );
+}
 
-//   if (error) return <div>failed to load</div>;
-//   if (!data) return <div>loading...</div>;
-
-//   return <div>hello {data[0].name}!</div>;
-
-//   const { airportdata, isLoading, isError } = useUser();
-
-//   if (isLoading) return <p>ロード中</p>;
-//   if (isError) return <p>エラー</p>;
-
-//   return (
-//     <div>
-//       <p>{airportdata[1].name}</p>;
-//       <Children theme={{ background: 'yellow' }} large />
-//     </div>
-//   );
-// }
-
-// export const Children = (props) => {
-//   return (
-//     <div>
-//       <p>Children Component</p>
-//       <style jsx>{`
-//         p {
-//           padding: ${'large' in props ? '50' : '20'}px;
-//           background: ${props.theme.background};
-//         }
-//       `}</style>
-//     </div>
-//   );
-// };
+export const Children = (props) => {
+  return (
+    <div>
+      <p>Children Component</p>
+      <style jsx>{`
+        p {
+          padding: ${'large' in props ? '50' : '20'}px;
+          background: ${props.theme.background};
+        }
+      `}</style>
+    </div>
+  );
+};

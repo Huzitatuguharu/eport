7c2c48fae5ea0e11cfaf96ea5f5c2b8278b6e7bb
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-09-20 20:20:35 +0900

    会社リスト表示

--- src/pages/components/pins.jsx
+++ src/pages/components/pins.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { IconContext } from 'react-icons';
 import { RiMapPin3Fill, RiMapPin3Line } from 'react-icons/ri';
 import { Marker } from 'react-map-gl';
-import { Loading } from '../components/Loading';
 import { useAirport } from '../hooks/useConnectSupabase';
 
 // Important for perf: the markers never change, avoid rerender when the map viewport changes
@@ -10,7 +9,7 @@ function Pins(props) {
   const { airportData, isLoading } = useAirport();
   const { onClick } = props;
 
-  if (isLoading) return <Loading />;
+  if (isLoading) return <p>ロード中</p>;
   if (airportData) {
     return airportData?.map((city, index) => (
       <Marker

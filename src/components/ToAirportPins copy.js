937fc769ab5a5333e116c4853677ef6809df5e8a
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-10-03 17:58:13 +0900

    rename

---
+++ src/components/ToAirportPins copy.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Marker } from 'react-map-gl';
+
+// アイコン
+const ICON =
+  'M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z';
+
+const pin_size_big = 20;
+
+export const ToAirportPins = (props) => {
+  const { toAirports, onClick } = props;
+
+  if (toAirports) {
+    return toAirports.map((city, index) => (
+      <Marker
+        key={`marker-${index}`}
+        longitude={city.airportlongitude}
+        latitude={city.airportlatitude}
+      >
+        <svg
+          height={pin_size_big}
+          viewBox='0 0 24 24'
+          style={{
+            cursor: 'pointer',
+            fill: '#59A4FD',
+            transform: `translate(${-pin_size_big / 2}px,${-pin_size_big + 7}px)`,
+          }}
+          onClick={() => onClick(city)}
+        >
+          <path d={ICON} />
+        </svg>
+      </Marker>
+    ));
+  }
+};
+
+React.memo(ToAirportPins);

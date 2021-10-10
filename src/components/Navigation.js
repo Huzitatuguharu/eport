4b81ecb35d49ee613354d80c0e00a8f4f5de797e
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-10-10 13:57:57 +0900

    akagechange

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,6 @@
 import Image from 'next/image';
 import * as React from 'react';
 
-const ICON =
-  'M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z';
-
-const pin_size_normal = 18;
 
 export const Navigation = (props) => {
   const { setClickAirport, setSelectedToAirport, setToAirportsData } = props;
